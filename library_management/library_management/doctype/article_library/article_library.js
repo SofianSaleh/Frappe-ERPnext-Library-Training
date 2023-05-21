fdfc5ba3f19481dff0e24a8696337df027192b83
SofianSaleh <sofiane.saleh11@gmail.com>
2023-05-21 14:35:37 +0200

    :fire: added a functionality to populate the articles using the openLibrary api + added an api end point to fetch all the articles

--- library_management/library_management/doctype/article_library/article_library.js
+++ library_management/library_management/doctype/article_library/article_library.js
@@ -3,6 +3,24 @@
 
 frappe.ui.form.on('Article Library', {
 	// refresh: function(frm) {
-
+		// frm.add_custom_button('action one', function(){
+			// console.log(frm.doc.isbn)
+			// frappe.call({
+			// 	method: 'library_management.library_management.doctype.article_library.api.get_publisher',
+			// 	args:{
+			// 		isbn: frm.doc.isbn,
+			// 	},
+			// 	callback: function(r){
+			// 		frm.article_name = r.message.title
+			// 		frm.author = r.message.author
+			// 		frm.publisher = r.message.publisher
+			// 		frm.image = r.message.image
+			// 		frm.route = r.message.name.split(' ').join('-')
+			// 		frm.save()
+			// 	}
+			// })
+		// })
+		
+		
 	// }
 });
